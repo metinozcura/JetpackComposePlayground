74f6d30d0964eb412ed105c2bb8aea909b0108f3
Metin Ozcura <metinozcura@gmail.com>
2021-02-27 21:05:12 +0300

    Simple list implementation.

--- app/src/main/java/com/metinozcura/jetpackcomposeplayground/MainActivity.kt
+++ app/src/main/java/com/metinozcura/jetpackcomposeplayground/MainActivity.kt
@@ -1,37 +1,70 @@
 package com.metinozcura.jetpackcomposeplayground
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.Divider
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.metinozcura.jetpackcomposeplayground.ui.theme.JetpackComposePlaygroundTheme
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            JetpackComposePlaygroundTheme {
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
-            }
+            DefaultPreview()
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun ListItem(txt: String) {
+    Row(modifier = Modifier.height(96.dp)) {
+        Image(
+            painter = painterResource(R.drawable.ic_launcher_foreground),
+            contentDescription = null,
+            modifier = Modifier
+                .width(96.dp)
+                .height(96.dp),
+            contentScale = ContentScale.Crop
+        )
+        Column(
+            modifier = Modifier
+                .padding(10.dp)
+                .fillMaxHeight(),
+            verticalArrangement = Arrangement.Center
+        ) {
+            Text(text = txt, color = Color.DarkGray)
+            Text(text = txt, color = Color.LightGray)
+        }
+    }
+}
+
+@Composable
+fun CreateList() {
+    LazyColumn {
+        items(100) { index ->
+            Log.i("mtn", index.toString())
+            ListItem(txt = index.toString())
+            Divider()
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     JetpackComposePlaygroundTheme {
-        Greeting("Android")
+        CreateList()
     }
 }
